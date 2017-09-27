a53ed8a5838a40ad6e8b8dcc186b034f6bc80e7f
Nicholas Gervais <gerva038@d.umn.edu>
2017-09-26 22:38:37 -0500

    changed file name. and added a simple (slow) ping sweep

--- pingsweep.py
+++ pingsweep.py
@@ -1,40 +1,12 @@
-import time
 import subprocess
-import multiprocessing.dummy
-import multiprocessing
-from multiprocessing.pool import Pool
 
-def ping(ip):
-    success = subprocess.call(['ping', '-n', '1', '-w', '15', ip], stdout=subprocess.PIPE)
-    if success == 0:
-        print("{} responded".format(ip))
-        file = open("pingReply.txt", "a")
-        file.write("1")
-        file.close()
-    else:
-        print("{} did not respond".format(ip))
+total_active = 0
 
-def ping_range():
-    totalActive = 0
-    num_threads = 3 * multiprocessing.cpu_count()
-    p = multiprocessing.dummy.Pool(num_threads)
-    for i in range(0, 256):
-        p.map(ping, ["131.212.%s.%s" % (i,x) for x in range(0, 256)])
+for x in range(0, 256):
+    for y in range(1, 256):
+        command = "ping -n 1 131.212.%s.%s -w 50" % (x,y)
+        res = subprocess.call(command)
+        if res == 0:
+            total_active += 1
 
-def countActive(latency):
-    chars = 0
-    with open('pingReply.txt', 'r') as in_file:
-        for line in in_file:
-            chars += len(line)
-    open('pingReply.txt', 'w').write('Total Active: ' + str(chars) + "\nLatency: " + str(latency))
-
-
-def main():
-    open('pingReply.txt', 'w').close()
-    start = time.time()
-    ping_range()
-    end = time.time()
-    latency = (end - start) / 60 / 60
-    countActive(latency)
-
-main()
+print (total_active)
